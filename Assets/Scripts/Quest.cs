7908535c9ce81121ee2daae1f00402bd3775e98e
maloun <u-user@inbox.ru>
2017-08-15 19:35:03 +0500

    first_commit

---
+++ Assets/Scripts/Quest.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Quest : MonoBehaviour {
+
+
+    public GameObject mark;
+    public Vector3 mark_pos;
+
+    public Quest()
+    {
+    }    
+
+
+}
